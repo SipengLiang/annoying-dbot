f34e18c5ae114f315524f4da62c5aaedd6298c2e
SipengLiang <38417993+SipengLiang@users.noreply.github.com>
2019-09-19 16:00:09 -0600

    Update b1.py

--- b1.py
+++ b1.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 from discord.ext.commands import Bot
 import asyncio
 
-TOKEN = 'NjIzMjM3OTk4NTI3NjQzNzEx.XX_igQ.cwYvNT1n6Ulr2JYjcV6UqNmHPAg'
+TOKEN = '#################'
 pause = False
 
 client = discord.Client()
